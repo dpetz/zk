ab92c4ecff8a06e0b882d2e9708389f4006e6948
dpetz <Dirk.Petzoldt@gmail.com>
2023-11-28 15:13:41 +0100

    basic markup parsing

--- src/zk/core.clj
+++ src/zk/core.clj
@@ -2,11 +2,10 @@
   (:require [clj-http.client :as http]
             [clojure.data.json :as json]
             [clojure.string :as str]
-            [clojure.walk :as walk] 
+            [clojure.walk :as walk]
             [cybermonday.core :as cyber]
             [clojure.spec.alpha :as s])
-  (:require [zk.model :as model] :reload)
-  ) ; (:use [clojure.tools.logging])
+  (:require [zk.model :as model] :reload)) ; (:use [clojure.tools.logging])
 
 ; https://joplinapp.org/help/api/references/rest_api/
 (def port "http://localhost:41184/")
@@ -26,10 +25,11 @@
 
 (defn if-single-key-drop-it
   "E.g. [{:title \"youtube\"} ..] to [\"youtube\" ..]. Otherwise unchanged."
-  [col keys]
-  (if-let [key (only keys)]
-    (map #(% key) col)
-    col))
+  [coll]
+  (if-let [key (only (keys (coll 0)))]
+    (map #(% key) coll)
+    coll))
+
 
 (defn syms-to-str [col] (str/join "," (map name col)))
 
@@ -58,25 +58,23 @@
              (walk/keywordize-keys  (json/read-str (:body response))) ; 
 
              {:keys [items has_more]}
-             (if (s/valid? :zk.model/response-items body) body {:items body :has_more 0})
+             (if (s/valid? :zk.model/response-items body) body {:items (vector body) :has_more false})
 
              cached
-             (concat cache (take gap (if-single-key-drop-it items fields)))
+             (concat cache (take gap (if-single-key-drop-it items)))
 
              gap-new
              (- gap (count items))]
 
-           (when INFO (println (str/upper-case verb)  body)) 
-           (if (and has_more (pos? gap-new))
-             (recur cached (update pars :page #(inc %)) gap-new)
-             (unwrap-if-alone  cached))))))
+         (when INFO (println (str/upper-case verb) body))
+         (if (and has_more (pos? gap-new))
+           (recur cached (update pars :page #(inc %)) gap-new)
+           (unwrap-if-alone  cached))))))
 
   ([verb fields]
    (auth-get verb fields {})))
 
 
-(let [m {1 2 3 4}] 
-  (if (map? m) m 0))
 
 ; ========== NOTES ==========
 
@@ -90,15 +88,19 @@
   ([] (notes note-keys)))
 
 (defn note [id]
-  (auth-get (str "notes/" id) [] {})); note-keys
+  (auth-get (str "notes/" id) note-keys)) 
+
+
 
 ; ========== TAGS ==========
 
+(def tag-keys
+  (model/spec-keys :zk.model/tag))
 
 (defn tags
   ([fields params]
    (auth-get "tags" fields params))
-  ([] (tags (model/spec-keys :zk.model/tag) {})))
+  ([] (tags tag-keys {})))
 
 
 
@@ -121,11 +123,15 @@
 ; ========== STAGE ==========
 
 
-(def book-id "f37effc03aa042399db702484c6cda61");
-
-(println (search "notebook:Meta")) ; title:Book
-;(println (note book-id))
 
 
+;(println (search "notebook:Meta")) ; title:Book
 
-  ;(cyber.core/parse-md my-markdown)
+
+(def author-id "19a0bb1074c9477b8204fb0d8e4baef5")
+(def book-id "f37effc03aa042399db702484c6cda61")
+(def book-body (note book-id))
+(def author-body (:body (note author-id)))
+
+;(println author-body)
+(pprint (cyber/parse-md author-body))
